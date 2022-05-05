49319d1bfd2b64f921e92476065281546b2ebfd4
Jordan McLeod <jordandmcleod@outlook.com>
2022-05-05 09:46:29 +0100

    Basic watermarking app UI created

--- watermarking/watermark.py
+++ watermarking/watermark.py
@@ -1,25 +1,71 @@
+from tkinter import *
+from tkinter import filedialog
 from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 
-photo = Image.open("images/photo-1.jpg")
+fonts = {
+    "Goldman": "Goldman-Regular.ttf",
+    "Inconsolata": "Inconsolata-Regular.ttf",
+    "WorkSans": "WorkSans-Regular.ttf"
+}
 
-w, h = photo.size
-print(w, h)
+def watermarking():
 
-drawing = ImageDraw.Draw(photo)
-font = ImageFont.truetype('fonts/WorkSans-Regular.ttf', 68)
+    photo = Image.open(upload_file_link.cget("text"))
 
-text = "© Jordan McLeod "
-text_w, text_h = drawing.textsize(text, font)
+    w, h = photo.size
+    print(w, h)
 
-pos = w - text_w, (h - text_h) - 50
+    drawing = ImageDraw.Draw(photo)
+    font = ImageFont.truetype(f'fonts/{fonts[clicked.get()]}', 40)
 
-c_text = Image.new('RGB', (text_w, (text_h)), color = '#000000')
-drawing = ImageDraw.Draw(c_text)
+    text = text_box.get()
+    text_w, text_h = drawing.textsize(text, font)
 
-drawing.text((0,0), text, fill="#ffffff", font=font)
-c_text.putalpha(100)
+    pos = (0,h-50)
 
-photo.paste(c_text, pos, c_text)
-photo.save('watermarked_images/watermarked_image.jpg')
+    c_text = Image.new('RGB', (text_w, (text_h)), color = '#000000')
+    drawing = ImageDraw.Draw(c_text)
+
+    drawing.text((0,0), text, fill="#ffffff", font=font)
+    c_text.putalpha(100)
+
+    photo.paste(c_text, pos, c_text)
+    photo.save('watermarked_images/watermarked_image.jpg')
+
+
+def UploadAction(event=None):
+    filename = filedialog.askopenfilename()
+    upload_file_link.config(text=filename)
+
+
+window = Tk()
+window.title("Watermarker")
+window.config(padx=100, pady=50)
+
+upload_file_label = Label(text="Please select a file to upload")
+upload_file_label.grid(column=0,row=0)
+upload_file = Button(window, text='Open', command=UploadAction)
+upload_file.grid(column=1,row=0)
+upload_file_link = Label(text="")
+upload_file_link.grid(column=0,row=1, columnspan=2)
+
+text_label = Label(text="What text would you like to set?")
+text_label.grid(column=0, row=2)
+text_box = Entry()
+text_box.grid(column=1, row=2)
+text_box.focus()
+
+clicked = StringVar()
+clicked.set("Goldman")
+
+font_selection_label = Label(text="What font would you like to use?")
+font_selection_label.grid(column=0, row=3)
+font_selection_dropdown = OptionMenu(window, clicked ,*fonts.keys())
+font_selection_dropdown.grid(column=1, row=3)
+
+submit = Button(text="Watermark!", command=watermarking)
+submit.grid(column=0, row=4, columnspan=2)
+
+window.mainloop()
